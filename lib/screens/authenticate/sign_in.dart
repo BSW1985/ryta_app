d945419eadc47c8d81140ad1c52fa2275032329c
Marek Fencl <mara.fencl@gmail.com>
2021-03-12 10:06:53 +0100

    sign_in screen basic design

--- lib/screens/authenticate/sign_in.dart
+++ lib/screens/authenticate/sign_in.dart
@@ -10,6 +10,10 @@ class _SignInState extends State<SignIn> {
 
   final AuthService _auth = AuthService();
 
+  // text field state
+  String email = '';
+  String password = '';
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -18,23 +22,70 @@ class _SignInState extends State<SignIn> {
       //   backgroundColor: Colors.blue,
       //   title: Text('New to Ryta? Sign in!'),
       // ),
-      body: ListView(
-        padding: EdgeInsets.symmetric(vertical: 230.0, horizontal: 70.0),
+      body: 
+      
+      ListView(
+        padding: EdgeInsets.symmetric(horizontal: 70.0),
         children: [
+          SizedBox(height: 150.0),
+
           Image.asset("assets/ryta_logo.png"),
 
-          ElevatedButton(
-          child: Text('Sign in anon'),
-          onPressed: () async {
-            dynamic result = await _auth.signInAnon();
-            if (result == null) {
-              print('error signing in');
-            } else {
-              print('signed in');
-              print(result.uid);
-            }
-          }
+          Form(
+            child: Column(
+              children: <Widget>[
+                SizedBox(height: 10.0),
+                TextFormField(
+                  decoration: InputDecoration(
+                    border:  OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.white, width: 1.0),
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.yellow[800], width: 3.0),
+                    ),
+                    hintText: 'Email',
+                    contentPadding: new EdgeInsets.symmetric(vertical: 10.0, horizontal: 10.0),
+                  ),
+                  onChanged: (val) {
+                    setState(() => email = val);
+                  }
+                ),
+                SizedBox(height: 10.0),
+                TextFormField(
+                  obscureText: true,
+                  decoration: InputDecoration(
+                    border:  OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.white, width: 1.0),
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.yellow[800], width: 3.0),
+                    ),
+                    hintText: 'Password',
+                    contentPadding: new EdgeInsets.symmetric(vertical: 10.0, horizontal: 10.0),
+                  ),
+                  onChanged: (val) {
+                    setState(() => password = val);
+                  }
+                ),
+                SizedBox(height: 20.0),
+                ElevatedButton(
+                  style: ButtonStyle(
+                    padding: MaterialStateProperty.all(EdgeInsets.symmetric(horizontal: 40.0, vertical: 15.0)),
+                    backgroundColor: MaterialStateProperty.all<Color>(Colors.yellow[800]) ),
+                  child: Text(
+                    'Sign in',
+                    //style: TextStyle(color: Colors.white),
+                    ),
+                  onPressed: () async {
+                    print(email);
+                    print(password);
+          
+                  }
+                  ),
+              ],
+            ),
           ),
+
         ],
       ),
     );
