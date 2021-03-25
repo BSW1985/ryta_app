125c6a2248d4ed7d5306986e9110524100bd3e41
Marek Fencl <mara.fencl@gmail.com>
2021-03-25 21:33:52 +0100

    Delete Goal Dialog, Communication with Usplash

---
+++ lib/config/keys.dart
@@ -0,0 +1,3 @@
+class Keys {
+  static const UNSPLASH_API_CLIENT_ID = "ask_Marek";
+}
--- lib/screens/home/goals_list.dart
+++ lib/screens/home/goals_list.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:ryta_app/config/keys.dart';
 import 'package:ryta_app/models/goal.dart';
 import 'package:ryta_app/models/user.dart';
+import 'package:ryta_app/screens/home/goal_definition.dart';
 import 'package:ryta_app/screens/home/goal_tile.dart';
 import 'package:ryta_app/services/database.dart';
 
@@ -17,9 +19,25 @@ class _GoalsListState extends State<GoalsList> {
     final goals = Provider.of<List<Goal>>(context);
     final user = Provider.of<RytaUser>(context);
 
+                  if (Keys.UNSPLASH_API_CLIENT_ID == "ask_Marek")
+                    return AlertDialog(
+                      title: Text('Ask Marek for Unsplash keys'),
+                      content: Text('Othewise it will not work'),
+                      actions: <Widget>[
+                        TextButton(
+                          child: Text('Mach ich!'),
+                          onPressed: (){
+                            // Hier passiert etwas
+                            // Navigator.of(context).pop();
+                          }
+                        ),
+                      ],
+                    );
     
     return Scaffold(
         body: 
+
+        
         
         ListView.builder(
         itemCount: goals == null ? 0 : goals.length,
@@ -30,6 +48,7 @@ class _GoalsListState extends State<GoalsList> {
         // add goal to Firebase
         floatingActionButton: FloatingActionButton(
               onPressed: () async {
+                Navigator.push(context, MaterialPageRoute(builder: (context) => GoalDefinition()));
                 DatabaseService(uid: user.uid).addUserGoals('new goal 4', 'IMAGE_URL'); 
               },
               child: Icon(Icons.add),
